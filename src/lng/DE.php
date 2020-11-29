7d9b522dbe9f55c7585c2cc5d49313d4f69d57a9
db-EV <github@elbil.rocks>
2020-11-29 17:42:18 +0100

    Create DE.php

---
+++ src/lng/DE.php
@@ -0,0 +1,33 @@
+<?php
+$weather_api_lng = 'de';
+$lng[1] = 'Aktualisieren';
+$lng[2] = 'Vorklimatisierung wurde angefordert.';
+$lng[3] = 'Sofortiges Laden wurde angefordert.';
+$lng[4] = 'Aktivierung des Ladeplaners wurde angefordert.';
+$lng[5] = 'Deaktivierung des Ladeplaners wurde angefordert.';
+$lng[6] = 'Es konnten keine neuen Daten abgerufen werden.';
+$lng[7] = 'Kilometerstand';
+$lng[8] = 'Angeschlossen';
+$lng[9] = 'Nein';
+$lng[10] = 'Ja';
+$lng[11] = 'Wird geladen';
+$lng[12] = 'Zeitnah';
+$lng[13] = 'Fertig';
+$lng[14] = 'Effekt';
+$lng[15] = 'Ladeplaner';
+$lng[16] = 'Ausgeschaltet';
+$lng[17] = 'Ausgeschaltet';
+$lng[18] = 'Akkustand';
+$lng[19] = 'Mail bei Akkustand';
+$lng[20] = 'Verf&uuml;gbare Energie';
+$lng[21] = 'Reichweite';
+$lng[22] = 'Akkutemperatur';
+$lng[23] = 'Au&szlig;entemperatur';
+$lng[24] = 'Statusupdate';
+$lng[25] = 'Fahrzeugposition';
+$lng[26] = 'Positionsupdate';
+$lng[27] = 'Vorklimatisierung starten';
+$lng[28] = 'ein';
+$lng[29] = 'aus';
+$lng[30] = 'Laden starten';
+?>
