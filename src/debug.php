bb1d291867ab9025093ad404b41564fe179939dd
db-EV <github@elbil.rocks>
2020-11-29 11:44:53 +0100

    Update debug.php

--- src/debug.php
+++ src/debug.php
@@ -30,6 +30,17 @@ $response = curl_exec($ch);
 if ($response === FALSE) die(curl_error($ch));
 echo 'cockpit: '.$response."\n\n";
 
+//Abfrage Ladeplanerstatus
+$postData = array(
+  'apikey: '.$kamereon_api,
+  'x-gigya-id_token: '.$session[1]
+);
+$ch = curl_init('https://api-wired-prod-1-euw1.wrd-aws.com/commerce/v1/accounts/'.$session[2].'/kamereon/kca/car-adapter/v1/cars/'.$vin.'/charge-mode?country='.$country);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $postData);
+$response = curl_exec($ch);
+echo 'charge-mode: '.$response."\n\n";
+
 //Abfrage Aussentemperatur
 $postData = array(
   'apikey: '.$kamereon_api,
