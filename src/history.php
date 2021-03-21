b140a1b25ca778714d483603cc468c43d8324e05
db-EV <github@elbil.rocks>
2021-03-21 11:27:54 +0100

    Update history.php

--- src/history.php
+++ src/history.php
@@ -27,19 +27,21 @@ $responseData = json_decode($response, TRUE);
 //Output
 echo '<HTML>'."\n".'<HEAD>'."\n".'<LINK REL="stylesheet" HREF="stylesheet.css">'."\n".'<META NAME="viewport" CONTENT="width=device-width, initial-scale=1.0">'."\n".'<TITLE>'.$zoename.'</TITLE>'."\n".'</HEAD>'."\n".'<BODY>'."\n".'<DIV ID="container">'."\n".'<MAIN>'."\n".'<ARTICLE>'."\n".'<TABLE>'."\n".'<TR ALIGN="left"><TH>'.$zoename.'</TH></TR>'."\n".'<TR><TD COLSPAN="2"><HR></TD></TR>'."\n";
 for ($i = 0; $i < count($responseData['data']['attributes']['charges']); $i++) {
-  $s = date_create_from_format(DATE_ISO8601, $responseData['data']['attributes']['charges'][$i]['chargeStartDate'], timezone_open('UTC'));
-  $s = date_timezone_set($s, timezone_open('Europe/Berlin'));
-  $s = date_format($s, 'd.m.Y H:i');
-  echo '<TR><TD>'.'Start'.':</TD><TD>'.$s.'</TD></TR>'."\n";
-  echo '<TR><TD>'.'Charging'.':</TD><TD>'.$responseData['data']['attributes']['charges'][$i]['chargeStartBatteryLevel'].' % to '.$responseData['data']['attributes']['charges'][$i]['chargeEndBatteryLevel'].' % in '.$responseData['data']['attributes']['charges'][$i]['chargeDuration'].' minutes</TD></TR>'."\n";
-  if ($zoeph == 1) {
-	$s = $responseData['data']['attributes']['charges'][$i]['chargeStartInstantaneousPower']/1000;
-    echo '<TR><TD>'.'Power'.':</TD><TD>'.$responseData['data']['attributes']['charges'][$i]['chargePower'].' ('.$s.' kW)</TD></TR>'."\n";
+  if (!empty($responseData['data']['attributes']['charges'][$i]['chargeStartDate'])) {
+    $s = date_create_from_format(DATE_ISO8601, $responseData['data']['attributes']['charges'][$i]['chargeStartDate'], timezone_open('UTC'));
+    $s = date_timezone_set($s, timezone_open('Europe/Berlin'));
+    $s = date_format($s, 'd.m.Y H:i');
+    echo '<TR><TD>'.'Start'.':</TD><TD>'.$s.'</TD></TR>'."\n";
+    echo '<TR><TD>'.'Charging'.':</TD><TD>'.$responseData['data']['attributes']['charges'][$i]['chargeStartBatteryLevel'].' % to '.$responseData['data']['attributes']['charges'][$i]['chargeEndBatteryLevel'].' % in '.$responseData['data']['attributes']['charges'][$i]['chargeDuration'].' minutes</TD></TR>'."\n";
+    if ($zoeph == 1) {
+	  $s = $responseData['data']['attributes']['charges'][$i]['chargeStartInstantaneousPower']/1000;
+      echo '<TR><TD>'.'Power'.':</TD><TD>'.$responseData['data']['attributes']['charges'][$i]['chargePower'].' ('.$s.' kW)</TD></TR>'."\n";
+    }
+    $s = date_create_from_format(DATE_ISO8601, $responseData['data']['attributes']['charges'][$i]['chargeEndDate'], timezone_open('UTC'));
+    $s = date_timezone_set($s, timezone_open('Europe/Berlin'));
+    $s = date_format($s, 'd.m.Y H:i');
+    echo '<TR><TD>'.'Status'.':</TD><TD>'.$responseData['data']['attributes']['charges'][$i]['chargeEndStatus'].' at '.$s.'</TD></TR>'."\n".'<TR><TD COLSPAN="2"><HR></TD></TR>'."\n";
   }
-  $s = date_create_from_format(DATE_ISO8601, $responseData['data']['attributes']['charges'][$i]['chargeEndDate'], timezone_open('UTC'));
-  $s = date_timezone_set($s, timezone_open('Europe/Berlin'));
-  $s = date_format($s, 'd.m.Y H:i');
-  echo '<TR><TD>'.'Status'.':</TD><TD>'.$responseData['data']['attributes']['charges'][$i]['chargeEndStatus'].' at '.$s.'</TD></TR>'."\n".'<TR><TD COLSPAN="2"><HR></TD></TR>'."\n";
 }
 echo '<TR><TD COLSPAN="2"><A HREF="./">'.'Back'.'</A></TD></TR>'."\n".'</TABLE>'."\n".'</ARTICLE>'."\n";
 echo '</MAIN>'."\n".'</DIV>'."\n".'</BODY>'."\n".'</HTML>';
