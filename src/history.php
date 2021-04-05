e63950a42188000678da5d7d095fe1327ad0c9f3
db-EV <github@elbil.rocks>
2021-04-05 10:41:06 +0200

    Update history.php

--- src/history.php
+++ src/history.php
@@ -8,9 +8,51 @@ header('Content-Type: text/html; charset=utf-8');
 if (empty(${$country})) $gigya_api = $GB;
 else $gigya_api = ${$country};
 
+$date_today = date_create('now');
+$date_today = date_format($date_today, 'md');
+$update_ok = FALSE;
+
 //Request cached login
 $session = file_get_contents('session');
 $session = explode('|', $session);
+
+//Retrieve new Gigya token if the session file is outdated
+if ($session[0] !== $date_today) {
+  //Login Gigya
+  $update_ok = TRUE;
+  $postData = array(
+    'ApiKey' => $gigya_api,
+    'loginId' => $username,
+    'password' => $password,
+    'include' => 'data',
+	'sessionExpiration' => 60
+  );
+  $ch = curl_init('https://accounts.eu1.gigya.com/accounts.login');
+  curl_setopt($ch, CURLOPT_POST, TRUE);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+  $response = curl_exec($ch);
+  if ($response === FALSE) die(curl_error($ch));  
+  $responseData = json_decode($response, TRUE);
+  $oauth_token = $responseData['sessionInfo']['cookieValue'];
+
+  //Request Gigya JWT token
+  $postData = array(
+    'login_token' => $oauth_token,
+    'ApiKey' => $gigya_api,
+    'fields' => 'data.personId,data.gigyaDataCenter',
+	'expiration' => 87000
+  );
+  $ch = curl_init('https://accounts.eu1.gigya.com/accounts.getJWT');
+  curl_setopt($ch, CURLOPT_POST, TRUE);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+  $response = curl_exec($ch);
+  if ($response === FALSE) die(curl_error($ch));
+  $responseData = json_decode($response, TRUE);
+  $session[1] = $responseData['id_token'];
+  $session[0] = $date_today;
+}
 
 //Request charging history
 $postData = array(
@@ -23,27 +65,37 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $postData);
 $response = curl_exec($ch);
 if ($response === FALSE) die(curl_error($ch));
 $responseData = json_decode($response, TRUE);
+if ($zoeph == 2) $data = array_reverse($responseData['data']['attributes']['charges']);
+else $data = $responseData['data']['attributes']['charges'];
 
 //Output
 echo '<HTML>'."\n".'<HEAD>'."\n".'<LINK REL="stylesheet" HREF="stylesheet.css">'."\n".'<META NAME="viewport" CONTENT="width=device-width, initial-scale=1.0">'."\n".'<TITLE>'.$zoename.'</TITLE>'."\n".'</HEAD>'."\n".'<BODY>'."\n".'<DIV ID="container">'."\n".'<MAIN>'."\n".'<ARTICLE>'."\n".'<TABLE>'."\n".'<TR ALIGN="left"><TH>'.$zoename.'</TH></TR>'."\n".'<TR><TD COLSPAN="2"><HR></TD></TR>'."\n";
-for ($i = 0; $i < count($responseData['data']['attributes']['charges']); $i++) {
-  if (!empty($responseData['data']['attributes']['charges'][$i]['chargeStartDate'])) {
-    $s = date_create_from_format(DATE_ISO8601, $responseData['data']['attributes']['charges'][$i]['chargeStartDate'], timezone_open('UTC'));
+for ($i = 0; $i < count($data); $i++) {
+  if (!empty($data[$i]['chargeStartDate']) && !empty($data[$i]['chargeEndDate'])) {
+    $s = date_create_from_format(DATE_ISO8601, $data[$i]['chargeStartDate'], timezone_open('UTC'));
     $s = date_timezone_set($s, timezone_open('Europe/Berlin'));
-    $s = date_format($s, 'd.m.Y H:i');
-    echo '<TR><TD>'.'Start'.':</TD><TD>'.$s.'</TD></TR>'."\n";
-    echo '<TR><TD>'.'Charging'.':</TD><TD>'.$responseData['data']['attributes']['charges'][$i]['chargeStartBatteryLevel'].' % to '.$responseData['data']['attributes']['charges'][$i]['chargeEndBatteryLevel'].' % in '.$responseData['data']['attributes']['charges'][$i]['chargeDuration'].' minutes</TD></TR>'."\n";
+	$sd = date_format($s, 'd.m.Y');
+	$st = date_format($s, 'H:i');
+	$s = date_create_from_format(DATE_ISO8601, $data[$i]['chargeEndDate'], timezone_open('UTC'));
+    $s = date_timezone_set($s, timezone_open('Europe/Berlin'));
+    $ed = date_format($s, 'd.m.Y');
+	$et = date_format($s, 'H:i');
+    echo '<TR><TD>'.'Start'.':</TD><TD>'.$sd.' '.$st.'</TD></TR>'."\n";
+    echo '<TR><TD>'.'Charging'.':</TD><TD>'.$data[$i]['chargeStartBatteryLevel'].' % to '.$data[$i]['chargeEndBatteryLevel'].' % in '.$data[$i]['chargeDuration'].' minutes</TD></TR>'."\n";
     if ($zoeph == 1) {
-	  $s = $responseData['data']['attributes']['charges'][$i]['chargeStartInstantaneousPower']/1000;
-      echo '<TR><TD>'.'Power'.':</TD><TD>'.$responseData['data']['attributes']['charges'][$i]['chargePower'].' ('.$s.' kW)</TD></TR>'."\n";
+	  $s = $data[$i]['chargeStartInstantaneousPower']/1000;
+      echo '<TR><TD>'.'Power'.':</TD><TD>'.$data[$i]['chargePower'].' ('.$s.' kW)</TD></TR>'."\n";
     }
-    $s = date_create_from_format(DATE_ISO8601, $responseData['data']['attributes']['charges'][$i]['chargeEndDate'], timezone_open('UTC'));
-    $s = date_timezone_set($s, timezone_open('Europe/Berlin'));
-    $s = date_format($s, 'd.m.Y H:i');
-    echo '<TR><TD>'.'Status'.':</TD><TD>'.$responseData['data']['attributes']['charges'][$i]['chargeEndStatus'].' at '.$s.'</TD></TR>'."\n".'<TR><TD COLSPAN="2"><HR></TD></TR>'."\n";
+    echo '<TR><TD>'.'Status'.':</TD><TD>'.$data[$i]['chargeEndStatus'].' at '.$ed.' '.$et.'</TD></TR>'."\n".'<TR><TD COLSPAN="2"><HR></TD></TR>'."\n";
   }
 }
 echo '<TR><TD COLSPAN="2"><A HREF="./">'.'Back'.'</A></TD></TR>'."\n".'</TABLE>'."\n".'</ARTICLE>'."\n";
 echo '</MAIN>'."\n".'</DIV>'."\n".'</BODY>'."\n".'</HTML>';
 curl_close($ch);
+
+//Cache new Gigya token
+if ($update_ok === TRUE) {
+  $session = implode('|', $session);
+  file_put_contents('session', $session);
+}
 ?>
