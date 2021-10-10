0dac1febd6ccfc2b0c68564a051ae3ec8e32d0aa
db-EV <github@elbil.rocks>
2021-10-10 08:12:55 +0200

    Added Gigya API key for Italy

--- src/api-keys.php
+++ src/api-keys.php
@@ -8,6 +8,8 @@ $AT = '3__B4KghyeUb0GlpU62ZXKrjSfb7CPzwBS368wioftJUL5qXE0Z_sSy0rX69klXuHy';
 $SE = '3_EN5Hcnwanu9_Dqot1v1Aky1YelT5QqG4TxveO0EgKFWZYu03WkeB9FKuKKIWUXIS';
 //United Kingdom
 $GB = '3_e8d4g4SE_Fo8ahyHwwP7ohLGZ79HKNN2T8NjQqoNnk6Epj6ilyYwKdHUyCw3wuxz';
+//Italia
+$IT = '3_js8th3jdmCWV86fKR3SXQWvXGKbHoWFv8NAgRbH7FnIBsi_XvCpN_rtLcI07uNuq';
 
 //Kamereon API key
 $kamereon_api = 'Ae9FDWugRxZQAGm3Sxgk7uJn6Q4CGEA2';
