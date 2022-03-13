c3d3f6db6e591d8e103776e26a903c4ffb177dd4
db-EV <github@elbil.rocks>
2022-03-13 19:26:30 +0100

    Added map provider and replaced curl_close
    Replacing curl_close to unset for PHP 8 compatibility

--- src/index.php
+++ src/index.php
@@ -289,71 +289,71 @@ if (isset($md5) && $md5 != $session[3] && $update_sucess === TRUE) {
     if ($response === FALSE) die(curl_error($ch));
     $responseData = json_decode($response, TRUE);
     $s = date_create_from_format(DATE_ISO8601, $responseData['data']['attributes']['lastUpdateTime'], timezone_open('UTC'));
-	if (empty($s)) $update_sucess = FALSE;
-	else {
+    if (empty($s)) $update_sucess = FALSE;
+    else {
       $s = date_timezone_set($s, timezone_open('Europe/Berlin'));
-	  $session[17] = $responseData['data']['attributes']['gpsLatitude'];
-	  $session[18] = $responseData['data']['attributes']['gpsLongitude'];
+      $session[17] = $responseData['data']['attributes']['gpsLatitude'];
+      $session[18] = $responseData['data']['attributes']['gpsLongitude'];
       $session[19] = date_format($s, 'd.m.Y');
-	  $session[20] = date_format($s, 'H:i');
-	}
+      $session[20] = date_format($s, 'H:i');
+    }
   }
   
   //Request weather data from openweathermap (only Ph2)
   if ($zoeph == 2 && $weather_api_key != '') {
-	$ch = curl_init('https://api.openweathermap.org/data/2.5/onecall/timemachine?lat='.$session[17].'&lon='.$session[18].'&dt='.$weather_api_dt.'&units=metric&lang='.$weather_api_lng.'&appid='.$weather_api_key);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-	$response = curl_exec($ch);
-	if ($response === FALSE) die(curl_error($ch));
-	$responseData = json_decode($response, TRUE);	
-	$session[22] = $responseData['current']['temp'];
-	$session[23] = $responseData['current']['weather']['0']['description'];
+    $ch = curl_init('https://api.openweathermap.org/data/2.5/onecall/timemachine?lat='.$session[17].'&lon='.$session[18].'&dt='.$weather_api_dt.'&units=metric&lang='.$weather_api_lng.'&appid='.$weather_api_key);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $response = curl_exec($ch);
+    if ($response === FALSE) die(curl_error($ch));
+    $responseData = json_decode($response, TRUE);	
+    $session[22] = $responseData['current']['temp'];
+    $session[23] = $responseData['current']['weather']['0']['description'];
   }
 
   //Send mail, execute command or activate schedule mode if configured
   if ($mail_bl === 'Y' || $cmon_bl === 'Y' || !empty($exec_bl)) {
     if ($session[12] >= $session[21] && $session[10] == 1 && $session[5] != 'Y') {
       if ($session[15] != '') $s = $session[15];
-	  else $s = $lng['some'];
+      else $s = $lng['some'];
       $sendmessage = $lng['Specified battery level reached.']."\n".$lng['Battery level'].': '.$session[12].' %'."\n".$lng['Remaining charging time'].': '.$s.' '.$lng['minutes']."\n".$lng['Range'].': '.$session[14].' km'."\n".$lng['Status update'].': '.$session[8].' '.$session[9];
-	  if ($mail_bl === 'Y') mail($username, $zoename, $sendmessage);
-	  if ($cmon_bl === 'Y') {
-	    $postData = array(
-	      'Content-type: application/vnd.api+json',
-	      'apikey: '.$kamereon_api,
-	      'x-gigya-id_token: '.$session[1]
-	    );
-	    $jsonData = '{"data":{"type":"ChargeMode","attributes":{"action":"schedule_mode"}}}';
-	    $ch = curl_init('https://api-wired-prod-1-euw1.wrd-aws.com/commerce/v1/accounts/'.$session[2].'/kamereon/kca/car-adapter/v1/cars/'.$vin.'/actions/charge-mode?country='.$country);
-	    curl_setopt($ch, CURLOPT_POST, TRUE);
-	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-	    curl_setopt($ch, CURLOPT_HTTPHEADER, $postData);
-	    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-	    $response = curl_exec($ch);
-	    if ($response === FALSE) die(curl_error($ch));
-	  }
+      if ($mail_bl === 'Y') mail($username, $zoename, $sendmessage);
+      if ($cmon_bl === 'Y') {
+        $postData = array(
+	  'Content-type: application/vnd.api+json',
+	  'apikey: '.$kamereon_api,
+	  'x-gigya-id_token: '.$session[1]
+	);
+	$jsonData = '{"data":{"type":"ChargeMode","attributes":{"action":"schedule_mode"}}}';
+	$ch = curl_init('https://api-wired-prod-1-euw1.wrd-aws.com/commerce/v1/accounts/'.$session[2].'/kamereon/kca/car-adapter/v1/cars/'.$vin.'/actions/charge-mode?country='.$country);
+	curl_setopt($ch, CURLOPT_POST, TRUE);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $postData);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+	$response = curl_exec($ch);
+	if ($response === FALSE) die(curl_error($ch));
+      }
       if (!empty($exec_bl)) shell_exec($exec_bl.' "'.$sendmessage.'"');
-	  $session[5] = 'Y';
-    } else if ($session[5] == 'Y' && $session[10] != 1) $session[5] = 'N';
+      $session[5] = 'Y';
+    } elseif ($session[5] == 'Y' && $session[10] != 1) $session[5] = 'N';
   }
   if ($mail_csf === 'Y' || !empty($exec_csf)) {
     $sendmessage = $lng['Charging finished.']."\n".$lng['Battery level'].': '.$session[12].' %'."\n".$lng['Range'].': '.$session[14].' km'."\n".$lng['Status update'].': '.$session[8].' '.$session[9];
     if ($session[6] == 'Y' && $session[10] != 1) {
-	  if ($mail_csf === 'Y') mail($username, $zoename, $sendmessage);
+      if ($mail_csf === 'Y') mail($username, $zoename, $sendmessage);
       if (!empty($exec_csf)) shell_exec($exec_bl.' "'.$sendmessage.'"');
-	}
-	if ($session[10] == 1) $session[6] = 'Y';
+    }
+    if ($session[10] == 1) $session[6] = 'Y';
     else $session[6] = 'N';
   }
 
   //Save data in database if configured
   if ($update_sucess === TRUE && $save_in_db === 'Y') {
     if (!file_exists('database.csv')) {
-	  if ($zoeph == 1) file_put_contents('database.csv', 'Date;Time;Mileage;Outside temperature;Battery temperature;Battery level;Range;Cable status;Charging status;Charging speed;Remaining charging time;Charging schedule'."\n");
+      if ($zoeph == 1) file_put_contents('database.csv', 'Date;Time;Mileage;Outside temperature;Battery temperature;Battery level;Range;Cable status;Charging status;Charging speed;Remaining charging time;Charging schedule'."\n");
       else file_put_contents('database.csv', 'Date;Time;Mileage;Battery level;Battery capacity;Range;Cable status;Charging status;Charging speed;Remaining charging time;GPS Latitude;GPS Longitude;GPS date;GPS time;Outside temperature;Weather condition;Charging schedule'."\n");
     }
     if ($zoeph == 1) file_put_contents('database.csv', $session[8].';'.$session[9].';'.$session[7].';'.$session[17].';'.$session[13].';'.$session[12].';'.$session[14].';'.$session[11].';'.$session[10].';'.$session[16].';'.$session[15].';'.$session[24]."\n", FILE_APPEND);
-	else file_put_contents('database.csv', $session[8].';'.$session[9].';'.$session[7].';'.$session[12].';'.$session[13].';'.$session[14].';'.$session[11].';'.$session[10].';'.$session[16].';'.$session[15].';'.$session[17].';'.$session[18].';'.$session[19].';'.$session[20].';'.$session[22].';'.$session[23].';'.$session[24]."\n", FILE_APPEND);
+    else file_put_contents('database.csv', $session[8].';'.$session[9].';'.$session[7].';'.$session[12].';'.$session[13].';'.$session[14].';'.$session[11].';'.$session[10].';'.$session[16].';'.$session[15].';'.$session[17].';'.$session[18].';'.$session[19].';'.$session[20].';'.$session[22].';'.$session[23].';'.$session[24]."\n", FILE_APPEND);
   }
 
   //Send data to ABRP if configured
@@ -367,14 +367,14 @@ if (isset($md5) && $md5 != $session[3] && $update_sucess === TRUE) {
     if ($response === FALSE) die(curl_error($ch));
   }
 }
-if (isset($ch)) curl_close($ch);
+if (isset($ch)) unset($ch);
 
 //Output
 if ($cmd_cron === TRUE) {
   if ($cmd_acnow === TRUE) echo 'AC NOW'."\n";
   if ($cmd_chargenow === TRUE) echo 'CHARGE NOW'."\n";
   if ($cmd_cmon === TRUE) echo 'CM ON'."\n";
-  else if ($cmd_cmoff === TRUE) echo 'CM OFF'."\n";
+  elseif ($cmd_cmoff === TRUE) echo 'CM OFF'."\n";
   if ($update_sucess === TRUE) echo 'OK';
   else echo 'NO DATA';
 } else {
@@ -385,45 +385,38 @@ if ($cmd_cron === TRUE) {
   if ($cmd_acnow === TRUE) echo '<TR><TD COLSPAN="2">'.$lng['Preconditioning requested.'].'</TD><TD>'."\n";
   if ($cmd_chargenow === TRUE) echo '<TR><TD COLSPAN="2">'.$lng['Instant charging requested.'].'</TD><TD>'."\n";
   if ($cmd_cmon === TRUE) echo '<TR><TD COLSPAN="2">'.$lng['Activation of the charging schedule requested.'].'</TD><TD>'."\n";
-  else if ($cmd_cmoff === TRUE) echo '<TR><TD COLSPAN="2">'.$lng['Deactivation of the charging schedule requested.'].'</TD><TD>'."\n";
+  elseif ($cmd_cmoff === TRUE) echo '<TR><TD COLSPAN="2">'.$lng['Deactivation of the charging schedule requested.'].'</TD><TD>'."\n";
   if ($update_sucess === FALSE && $update_ok === TRUE) echo '<TR><TD COLSPAN="2">'.$lng['No new data'].'</TD><TD>'."\n";
     echo '<TR><TD>'.$lng['Mileage'].':</TD><TD>'.$session[7].' km</TD></TR>'."\n".'<TR><TD>'.$lng['Connected'].':</TD><TD>';
-    if ($session[11] == 0){
-      echo $lng['No'];
-    } else {
-      echo $lng['Yes'];
-    }
+    if ($session[11] == 0) echo $lng['No'];
+    else echo $lng['Yes'];
     echo '</TD></TR>'."\n".'<TR><TD>'.$lng['Charging'].':</TD><TD>';
     if ($session[10] == 1){
-	  if ($session[15] != ''){
-        $s = date_create_from_format('d.m.YH:i', $session[8].$session[9]);
-        date_add($s, date_interval_create_from_date_string($session[15].' minutes'));
-        $s = date_format($s, 'H:i');
-      } else $s = $lng['Soon'];
-      echo $lng['Yes'].'</TD></TR>'."\n".'<TR><TD>'.$lng['Ready'].':</TD><TD>'.$s;
-	  if ($zoeph == 1) echo '</TD></TR>'."\n".'<TR><TD>'.$lng['Effect'].':</TD><TD>'.$session[16].' kW';
-    } else {
-      echo $lng['No'];
-    }
-	if ($hide_cm !== 'Y') {
-	  echo '</TD></TR>'."\n".'<TR><TD>'.$lng['Charging schedule'].':</TD><TD>';
-	  if (substr($session[24], 0, 6) === 'always' || $session[24] === 'n/a') echo $lng['Inactive'];
-	  else echo $lng['Active'];
+      if ($session[15] != ''){
+      $s = date_create_from_format('d.m.YH:i', $session[8].$session[9]);
+      date_add($s, date_interval_create_from_date_string($session[15].' minutes'));
+      $s = date_format($s, 'H:i');
+    } else $s = $lng['Soon'];
+    echo $lng['Yes'].'</TD></TR>'."\n".'<TR><TD>'.$lng['Ready'].':</TD><TD>'.$s;
+    if ($zoeph == 1) echo '</TD></TR>'."\n".'<TR><TD>'.$lng['Effect'].':</TD><TD>'.$session[16].' kW';
+    else echo $lng['No'];
+    if ($hide_cm !== 'Y') {
+      echo '</TD></TR>'."\n".'<TR><TD>'.$lng['Charging schedule'].':</TD><TD>';
+      if (substr($session[24], 0, 6) === 'always' || $session[24] === 'n/a') echo $lng['Inactive'];
+      else echo $lng['Active'];
     }
     echo '</TD></TR>'."\n".'<TR><TD>'.$lng['Battery level'].':</TD><TD>'.$session[12].' %</TD></TR>'."\n";
-	if ($mail_bl === 'Y' || $cmon_bl === 'Y' || !empty($exec_bl)) echo '<TR><TD>'.$lng['Action at battery level'].':</TD><TD><INPUT TYPE="number" NAME="bl" VALUE="'.$session[21].'" MIN="1" MAX="99"><INPUT TYPE="submit" VALUE="%"></TD></TR>'."\n";
-    if ($zoeph == 2) {
-      echo '<TR><TD>'.$lng['Battery capacity'].':</TD><TD>'.$session[13].' kWh</TD></TR>'."\n";
-    }
+    if ($mail_bl === 'Y' || $cmon_bl === 'Y' || !empty($exec_bl)) echo '<TR><TD>'.$lng['Action at battery level'].':</TD><TD><INPUT TYPE="number" NAME="bl" VALUE="'.$session[21].'" MIN="1" MAX="99"><INPUT TYPE="submit" VALUE="%"></TD></TR>'."\n";
+    if ($zoeph == 2) echo '<TR><TD>'.$lng['Battery capacity'].':</TD><TD>'.$session[13].' kWh</TD></TR>'."\n";
     echo '<TR><TD>'.$lng['Range'].':</TD><TD>'.$session[14].' km</TD></TR>'."\n";
-    if ($zoeph == 1) {
-      echo '<TR><TD>'.$lng['Battery temperature'].':</TD><TD>'.$session[13].' &deg;C</TD></TR>'."\n".'<TR><TD>'.$lng['Outside temperature'].':</TD><TD>'.$session[17].' &deg;C</TD></TR>'."\n";
-    } else {
-	  if ($weather_api_key != '') echo '<TR><TD>'.$lng['Outside temperature'].':</TD><TD>'.$session[22].' &deg;C ('.htmlentities($session[23]).')</TD></TR>'."\n";
-	}
+    if ($zoeph == 1) echo '<TR><TD>'.$lng['Battery temperature'].':</TD><TD>'.$session[13].' &deg;C</TD></TR>'."\n".'<TR><TD>'.$lng['Outside temperature'].':</TD><TD>'.$session[17].' &deg;C</TD></TR>'."\n";
+    elseif ($weather_api_key != '') echo '<TR><TD>'.$lng['Outside temperature'].':</TD><TD>'.$session[22].' &deg;C ('.htmlentities($session[23]).')</TD></TR>'."\n";
     echo '<TR><TD>'.$lng['Status update'].':</TD><TD>'.$session[8].' '.$session[9].'</TD></TR>'."\n";
     if ($zoeph == 2) {
-      echo '<TR><TD>'.$lng['Car position'].':</TD><TD><A HREF="https://www.google.com/maps/place/'.$session[17].','.$session[18].'" TARGET="_blank">Google Maps</A></TD></TR>'."\n".'<TR><TD>'.$lng['Position update'].':</TD><TD>'.$session[19].' '.$session[20].'</TD></TR>'."\n";
+      echo '<TR><TD>'.$lng['Car position'].':</TD><TD>';
+      if ($map_provider == 'osm') echo '<A HREF="https://www.openstreetmap.org/?mlat='.$session[17].'&mlon='.$session[18].'&zoom=18" TARGET="_blank">OpenStreetMap</A>';
+      else echo '<A HREF="https://www.google.com/maps/place/'.$session[17].','.$session[18].'" TARGET="_blank">Google Maps</A>';
+      echo '</TD></TR>'."\n".'<TR><TD>'.$lng['Position update'].':</TD><TD>'.$session[19].' '.$session[20].'</TD></TR>'."\n";
     }
   echo '<TR><TD COLSPAN="2"><A HREF="'.$requesturi.'?acnow">'.$lng['Start preconditioning'].'</A></TD></TR>'."\n";
   if ($hide_cm !== 'Y') echo '<TR><TD COLSPAN="2">'.$lng['Charging schedule'].': <A HREF="'.$requesturi.'?cmon">'.$lng['on'].'</A> | <A HREF="'.$requesturi.'?cmoff">'.$lng['off'].'</A></TD></TR>'."\n".'<TR><TD COLSPAN="2"><A HREF="'.$requesturi.'?chargenow">'.$lng['Start charging'].'</A></TD></TR>'."\n";
