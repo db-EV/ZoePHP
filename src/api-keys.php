d62687648e09cfd34b2686692bcef64b4ed04f78
db-EV <28677065+db-EV@users.noreply.github.com>
2026-02-22 09:30:15 +0100

    Update api-keys.php

--- src/api-keys.php
+++ src/api-keys.php
@@ -1,16 +1,14 @@
 <?php
-// Gigya API keys (depending on your country)
-//Deutschland
-$DE = '3_7PLksOyBRkHv126x5WhHb-5pqC1qFR8pQjxSeLB6nhAnPERTUlwnYoznHSxwX668';
-//Österreich
-$AT = '3__B4KghyeUb0GlpU62ZXKrjSfb7CPzwBS368wioftJUL5qXE0Z_sSy0rX69klXuHy';
-//Sverige
-$SE = '3_EN5Hcnwanu9_Dqot1v1Aky1YelT5QqG4TxveO0EgKFWZYu03WkeB9FKuKKIWUXIS';
-//United Kingdom
-$GB = '3_e8d4g4SE_Fo8ahyHwwP7ohLGZ79HKNN2T8NjQqoNnk6Epj6ilyYwKdHUyCw3wuxz';
-//Italia
-$IT = '3_js8th3jdmCWV86fKR3SXQWvXGKbHoWFv8NAgRbH7FnIBsi_XvCpN_rtLcI07uNuq';
+/**
+ * Gigya API keys per country, and Kamereon API key.
+ */
 
-//Kamereon API key
+$gigya_keys = [
+    'DE' => '3_7PLksOyBRkHv126x5WhHb-5pqC1qFR8pQjxSeLB6nhAnPERTUlwnYoznHSxwX668',
+    'AT' => '3__B4KghyeUb0GlpU62ZXKrjSfb7CPzwBS368wioftJUL5qXE0Z_sSy0rX69klXuHy',
+    'SE' => '3_EN5Hcnwanu9_Dqot1v1Aky1YelT5QqG4TxveO0EgKFWZYu03WkeB9FKuKKIWUXIS',
+    'GB' => '3_e8d4g4SE_Fo8ahyHwwP7ohLGZ79HKNN2T8NjQqoNnk6Epj6ilyYwKdHUyCw3wuxz',
+    'IT' => '3_js8th3jdmCWV86fKR3SXQWvXGKbHoWFv8NAgRbH7FnIBsi_XvCpN_rtLcI07uNuq',
+];
+
 $kamereon_api = 'YjkKtHmGfaceeuExUDKGxrLZGGvtVS0J';
-?>
