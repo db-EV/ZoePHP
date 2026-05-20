b2fda3d459d03f506e03d9f09b1c8b44efcbc093
db-EV <28677065+db-EV@users.noreply.github.com>
2026-05-20 17:43:58 +0200

    Change Gigya API key
    Changed from country depended API keys to the last Gigya API key for Europe

--- src/api-keys.php
+++ src/api-keys.php
@@ -1,14 +1,8 @@
 <?php
 /**
- * Gigya API keys per country, and Kamereon API key.
+ * Gigya API key for Europe, and Kamereon API key.
  */
 
-$gigya_keys = [
-    'DE' => '3_7PLksOyBRkHv126x5WhHb-5pqC1qFR8pQjxSeLB6nhAnPERTUlwnYoznHSxwX668',
-    'AT' => '3__B4KghyeUb0GlpU62ZXKrjSfb7CPzwBS368wioftJUL5qXE0Z_sSy0rX69klXuHy',
-    'SE' => '3_EN5Hcnwanu9_Dqot1v1Aky1YelT5QqG4TxveO0EgKFWZYu03WkeB9FKuKKIWUXIS',
-    'GB' => '3_e8d4g4SE_Fo8ahyHwwP7ohLGZ79HKNN2T8NjQqoNnk6Epj6ilyYwKdHUyCw3wuxz',
-    'IT' => '3_js8th3jdmCWV86fKR3SXQWvXGKbHoWFv8NAgRbH7FnIBsi_XvCpN_rtLcI07uNuq',
-];
+$gigya_key = '3_VgdkgtIRH3AdHvJm-cjV2ug2EFE0lxt0IJzMC4MFqZjFpn_GYFXVdNZ19L7wZX0N';
 
 $kamereon_api = 'YjkKtHmGfaceeuExUDKGxrLZGGvtVS0J';
