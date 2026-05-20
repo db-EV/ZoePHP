8d12262fd1e91d59226da406596297dd17e68b38
db-EV <28677065+db-EV@users.noreply.github.com>
2026-05-20 17:49:57 +0200

    Change Gigya API key

--- src/api-keys.php
+++ src/api-keys.php
@@ -3,6 +3,6 @@
  * Gigya API key for Europe, and Kamereon API key.
  */
 
-$gigya_key = '3_VgdkgtIRH3AdHvJm-cjV2ug2EFE0lxt0IJzMC4MFqZjFpn_GYFXVdNZ19L7wZX0N';
+$gigya_api = '3_VgdkgtIRH3AdHvJm-cjV2ug2EFE0lxt0IJzMC4MFqZjFpn_GYFXVdNZ19L7wZX0N';
 
 $kamereon_api = 'YjkKtHmGfaceeuExUDKGxrLZGGvtVS0J';
